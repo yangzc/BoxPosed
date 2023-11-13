c81417dcb4b810b830515e4fd47f62e08ecdd4d2
yangzc <zhc_yang@163.com>
2023-11-13 16:08:03 +0800

    modify setting for jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.lsplugin.cmaker)
+//    alias(libs.plugins.lsplugin.cmaker)
     alias(libs.plugins.application) apply false
     alias(libs.plugins.library) apply false
     alias(libs.plugins.kotlin) apply false
